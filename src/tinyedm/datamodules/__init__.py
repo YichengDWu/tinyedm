02f8a0a9f3199b52a2484eaa7aa2415fdd881223
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2024-01-24 00:54:47 +0800

    fix

--- src/tinyedm/datamodules/__init__.py
+++ src/tinyedm/datamodules/__init__.py
@@ -2,6 +2,5 @@ from .abstract_datamodule import AbstractDataModule
 from .cifar10datamodule import CIFAR10DataModule
 from .mnistdatamodule import MNISTDataModule
 from .random_datamodule import RandomNoiseDataModule
-from .imagenet_datamodule import ImagenetDataModule
 
 
